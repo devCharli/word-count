470779b8069f356a084b936d2820e367f3116d3a
devCharli <devcharli@gmail.com>
2024-03-07 14:28:44 -0800

    add container main css

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,3 +1,10 @@
 export default function Container() {
-  return <main>Container</main>;
+  return (
+    <main
+      className="flex flex-col sm:flex-row sm:w-[1050px] h-[520px] bg-[#fff] 
+   rounded-[10px] mt-[55px] overflow relative shadow-[0_4px_4px_rgba(0,0,0,0.3)]"
+    >
+      Container
+    </main>
+  );
 }
