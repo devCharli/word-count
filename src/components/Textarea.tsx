07e1d529fda869367537e2d0191aeafe03f64b89
devCharli <devcharli@gmail.com>
2024-03-07 14:47:13 -0800

    implement textarea for text control input handling

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,7 +1,22 @@
-export default function Textarea() {
+type TextAreaProp = {
+  text: string;
+  setText: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export default function Textarea({ text, setText }: TextAreaProp) {
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+    setText(newText);
+  };
   return (
-    <div>
-      <textarea placeholder="Enter your text" />
+    <div className="text-[22px] py-[30px] px-[40px] sm:flex-2 resize-none">
+      <textarea
+        className="text-[#959c9f] font-medium h-full w-full"
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck="false"
+      />
     </div>
   );
 }
